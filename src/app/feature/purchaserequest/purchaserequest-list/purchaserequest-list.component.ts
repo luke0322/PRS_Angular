bd7e04d46241e20cfa06e480f94b9ad0049eabf8
Lucas Hinegardner <lucas.hinegardner@gmail.com>
2018-01-13 19:22:35 -0500

    added new fun to pr'

--- src/app/feature/purchaserequest/purchaserequest-list/purchaserequest-list.component.ts
+++ src/app/feature/purchaserequest/purchaserequest-list/purchaserequest-list.component.ts
@@ -3,6 +3,10 @@ import { PurchaserequestService } from '../../../service/purchaserequest.service
 import { PurchaseRequest } from '../../../model/purchaserequest';
 import { UserService } from '../../../service/user.service';
 import { User } from '../../../model/user';
+import { LineItemService } from '../../../service/lineitem.service';
+import { ProductService } from '../../../service/product.service';
+import { Product } from '../../../model/product';
+
 
 @Component({
   selector: 'app-purchaserequest-list',
@@ -16,8 +20,50 @@ export class PurchaserequestListComponent implements OnInit {
   sortDesc: string = 'asc';
   sortKeys: string[] = PurchaseRequest.sortableKeys;
   purchaseRequests: PurchaseRequest[];
+  product: Product;
   
-  constructor(private PurchaseRequestSvc: PurchaserequestService) { }
+  appendLineItems(event){
+    event.preventDefault();
+    let purchaseId: any = (event.srcElement.name.substring(event.srcElement.name.length-1, event.srcElement.name.length));
+    // var parentRequest = document.getElementById("purchaseRequest"+purchaseId);
+    this.LineItemSvc.lines(purchaseId)
+    .subscribe(prlis=>{
+      var parentRequest = document.getElementById("purchaseRequest"+purchaseId);
+      if(prlis.length > 0){
+        for (var i = 0; i < prlis.length; ++i){
+          
+          let producthead = document.createElement("th");
+          let pricehead = document.createElement("th");
+          let quantityhead = document.createElement("th");
+          let row = document.createElement("tr");
+          let name = document.createElement("td");
+          let price = document.createElement("td");
+          let quantity = document.createElement("td");
+          quantity.innerHTML = prlis[i].Quantity.toString();
+          this.ProductSvc.get(prlis[i].ProductID)
+          .subscribe(prod=>{
+            let ourproduct =  prod.length > 0 ? prod[0]: null;
+            name.innerHTML = ourproduct.Name;
+            price.innerHTML = ourproduct.Price.toString();
+            row.appendChild(name);
+            row.appendChild(price);
+            row.appendChild(quantity);
+            parentRequest.appendChild(row);
+          });
+          // name.innerHTML = prlis[i].ProductSvc.
+        }
+      }else if (prlis.length == 0){
+
+        let emptyPRLIS = document.createElement("tr")
+        emptyPRLIS.innerHTML = "This purchase request has no line items"
+        parentRequest.appendChild(emptyPRLIS);
+
+      }
+    });
+  }
+  constructor(private PurchaseRequestSvc: PurchaserequestService,
+              private LineItemSvc: LineItemService,
+              private ProductSvc: ProductService) { }
 
   ngOnInit() {
   	this.PurchaseRequestSvc.list()
